59f50a317e16b082a9fa35327bb7d1795ea6dba9
sofiimartinex <sofiimartinex@icloud.com>
2026-02-24 09:42:01 +0100

    main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, ChatJoinRequestHandler
 
 TOKEN = "8485925922:AAFM9EcljRLHF_1MxlFBm24SXrC2zko0DUc"
-PHOTO_URL = "https://TUO_LINK_IMMAGINE.jpg"
+PHOTO_URL = ""C:\Users\Francesco\Downloads\sofia.jpg""
 TEXT = (
     "Ciaooo 😘\n\n"
     "La tua richiesta è stata approvata!\n\n"
-    "🎁 Clicca qui per il regalo:\n"
-    "👉 https://TUO_LINK_ESTERNO.com"
+    "🎁 Clicca qui per il tuo regalo: è gratis!\n"
+    "👉https://onlyfans.com/sofiaitalia2006/c47"
 )
 
 async def handle_join_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
